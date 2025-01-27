97d442b3ff7bf620795bb42e6c67c53b4470c99a
SaifAllahGharib <saifallahgharib@gmail.com>
2025-01-27 15:59:06 +0200

    finish custom item sliver list view main buyer view

--- lib/features/home/presentation/view/widgets/custom_item_sliver_list_view_buyer_view.dart
+++ lib/features/home/presentation/view/widgets/custom_item_sliver_list_view_buyer_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:yummy_home/core/utils/colors.dart';
 import 'package:yummy_home/core/utils/dimensions.dart';
+import 'package:yummy_home/core/utils/styles.dart';
+import 'package:yummy_home/core/widgets/custom_row_cost.dart';
 
 class CustomItemSliverListViewBuyerView extends StatelessWidget {
   const CustomItemSliverListViewBuyerView({super.key});
@@ -28,6 +30,72 @@ class CustomItemSliverListViewBuyerView extends StatelessWidget {
                 image: AssetImage("assets/images/person.jpg"),
               ),
             ),
+          ),
+          SizedBox(width: Dimensions.height15(context)),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Bazoka",
+                style: Styles.textStyle18(context),
+              ),
+              SizedBox(height: Dimensions.height15(context)),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star,
+                    color: Colors.yellow,
+                    size: Dimensions.height20(context),
+                  ),
+                  SizedBox(width: Dimensions.height10(context) * 0.5),
+                  Text(
+                    "4.8",
+                    style: Styles.textStyle15(context)
+                        .copyWith(fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(width: Dimensions.height10(context) * 0.5),
+                  Text(
+                    "(245)",
+                    style: Styles.textStyle15(context)
+                        .copyWith(color: Colors.grey),
+                  ),
+                ],
+              ),
+              SizedBox(height: Dimensions.height10(context) * 0.6),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.access_time_rounded,
+                    size: Dimensions.height20(context),
+                    color: Colors.grey,
+                  ),
+                  SizedBox(width: Dimensions.height10(context) * 0.5),
+                  Text(
+                    "24m",
+                    style: Styles.textStyle15(context)
+                        .copyWith(color: Colors.grey),
+                  ),
+                  SizedBox(width: Dimensions.height10(context) * 0.5),
+                  Icon(
+                    Icons.circle,
+                    size: Dimensions.height10(context) * 0.7,
+                    color: Colors.black54,
+                  ),
+                  SizedBox(width: Dimensions.height10(context) * 0.5),
+                  Icon(
+                    Icons.motorcycle_outlined,
+                    size: Dimensions.height15(context),
+                    color: Colors.black54,
+                  ),
+                  SizedBox(width: Dimensions.height10(context) * 0.5),
+                  CustomRowCost(
+                    egp: "15.99",
+                    color: Colors.grey,
+                  ),
+                ],
+              ),
+            ],
           ),
         ],
       ),
